f9bd898288278d3da4c7b32df882cac035a03168
stuffrome <gabeperez018@gmail.com>
2019-03-02 00:47:55 -0500

    Login buttons started

--- server/config/express.js
+++ server/config/express.js
@@ -3,8 +3,7 @@ var path = require('path'),
     mongoose = require('mongoose'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
-    config = require('./config'),
-    listingsRouter = require('../routes/listings.server.routes');
+    config = require('./config');
 
 module.exports.init = function() {
   //connect to database
@@ -18,18 +17,9 @@ module.exports.init = function() {
 
   //body parsing middleware 
   app.use(bodyParser.json());
-
   
-  /**TODO
-  Serve static files */
   app.use('/', express.static('client'));
 
-  /**TODO 
-  Use the listings router for requests to the api */
-  app.use('/api/listings', listingsRouter);
-
-  /**TODO 
-  Go to homepage for all routes not specified */ 
   app.get('*', function(req, res) {
     res.sendFile(path.resolve('./client/index.html'));
   });
