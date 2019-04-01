61aaf867648d4cb27977e4e405a81cb578684ac1
stuffrome <gabeperez018@gmail.com>
2019-04-01 13:58:44 -0400

    Registration requires password confirmation & Login visual validation features added & Topic's tweets link to real tweet on Twitter

--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -4,11 +4,15 @@ angular.module('login')
             $scope.message = "Please login with your account or register.";
 
             $scope.login = function() {
+
+                $scope.invalidLogin = false;
+                $scope.message = "Logging you in...";
+
                 authService.login($scope.credentials).then(function(res) {
-                    $scope.message = "Success";
                     $rootScope.currentUser = $scope.credentials.email;
                     $location.path("/dashboard");
                 }, function(err) {
+                    $scope.invalidLogin = true;
                     $scope.message = "Invalid username/password";
                 })
             }
